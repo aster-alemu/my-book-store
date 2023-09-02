4b1211e59e92f8daa59f1aa2ab5fa37b47d826cf
Aster Alemu <leaster2014w@gmail.com>
2023-09-02 11:43:05 +0300

    Add files

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Categories.css';
 
 const Categories = () => (
-  <div className="container">
-    <h3>Under Construction</h3>
+  <div>
+    <h3>Categories</h3>
   </div>
 );
+
 export default Categories;
